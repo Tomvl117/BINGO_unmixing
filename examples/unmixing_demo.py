027702fbae9b7ba87a08796545f7b2430c90a41e
Tom van Leeuwen <tom@tleeuwen.nl>
2025-07-29 16:54:06 +0200

    Change example to reflect new fit_transform

--- examples/unmixing_demo.py
+++ examples/unmixing_demo.py
@@ -1,11 +1,9 @@
 from src.bingo_unmixing.bingo_nmf import BINGONMF
 from src.bingo_unmixing.bingo_nmf_gpu import BINGONMF_GPU
 import tifffile
-import numpy as np
 
 
 img = tifffile.imread(r"Z:\Rheenen\tvl_jr\normal Merged crop-2.tif")
-max_value = np.iinfo(img.dtype).max
 
 model = BINGONMF(
     n_components=img.shape[0],
@@ -22,31 +20,18 @@ model_gpu = BINGONMF_GPU(
     max_iter=300,
 )
 
-if img.ndim < 4:
-    C = img.shape[0]
-    X = img.reshape(C, -1).T
-# else: 3D application
-
-X = X.astype(float)/max_value
-
 import time
 
 time_start = time.time()
-W_cpu = model.fit_transform(X)
+W_cpu = model.fit_transform(img)
 print("CPU finished in ", time.time() - time_start)
 
 time_start = time.time()
-W_gpu = model_gpu.fit_transform(X)
+W_gpu = model_gpu.fit_transform(img)
 print("GPU finished in ", time.time() - time_start)
 
 # Output CPU calculated
-output = W_cpu.T.reshape(img.shape)
-output_img = (output*max_value).astype(img.dtype)
-
-tifffile.imwrite(r"Z:\Rheenen\tvl_jr\unmixed_cpu.tif", output_img)
+tifffile.imwrite(r"Z:\Rheenen\tvl_jr\unmixed_cpu.tif", W_cpu)
 
 # Output GPU calculated
-output = W_gpu.T.reshape(img.shape)
-output_img = (output*max_value).astype(img.dtype)
-
-tifffile.imwrite(r"Z:\Rheenen\tvl_jr\unmixed_gpu.tif", output_img)
+tifffile.imwrite(r"Z:\Rheenen\tvl_jr\unmixed_gpu.tif", W_gpu)
